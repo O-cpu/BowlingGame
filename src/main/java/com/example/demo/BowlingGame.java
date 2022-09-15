0c20e38ce6d095c735cfb3a8ea60e93800e874e6
O-cpu <60776480+O-cpu@users.noreply.github.com>
2022-09-15 16:35:37 +0200

    + Unit Tests

---
+++ src/main/java/com/example/demo/BowlingGame.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class BowlingGame {
+
+    public static int[] getResult(int[] rolls)
+    {
+        return new int[10];
+    }
+}
--- src/test/java/com/example/demo/BowlingApplicationTests.java
+++ src/test/java/com/example/demo/BowlingApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,10 @@ class BowlingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int[] rolls = {1,4,4,5,6,4,5,5,0,10,0,1,7,3,6,4,0,10,2,8,6};
+		int[] expectedResult = {5,14,29,49,60,61,77,97,117,133};
+		Assert.assertArrayEquals(expectedResult, BowlingGame.getResult(rolls));
+
 	}
 
 }
