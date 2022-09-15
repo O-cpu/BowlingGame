ac0c0f981283efb94213b71e44ce5ee1046e5ee6
O-cpu <60776480+O-cpu@users.noreply.github.com>
2022-09-15 17:21:06 +0200

    First implementation

--- src/main/java/com/example/demo/BowlingGame.java
+++ src/main/java/com/example/demo/BowlingGame.java
@@ -4,6 +4,45 @@ public class BowlingGame {
 
     public static int[] getResult(int[] rolls)
     {
-        return new int[10];
+
+        if (rolls.length!=21)
+        {
+            throw new IllegalArgumentException("Wrong rolls array size. Must be 21.");
+        }
+
+        int[] result = new int[10];
+        for (int i=0; i<19; i+=2)
+        {
+            int firstRoll = rolls[i];
+            int secondRoll = rolls[i+1];
+            if (firstRoll>10 || firstRoll<0 || secondRoll>10 || secondRoll<0 ||
+                    (firstRoll == 10 && secondRoll !=0) ||
+                    (firstRoll + secondRoll >10 ) )
+            {
+                throw new IllegalArgumentException("Wrong rolls data. Please check game rules");
+            }
+
+            if (i != 18) {
+                // Normal Frame
+                if (firstRoll == 10) {
+                    result[i / 2] = rolls[i] + ((rolls[i + 2] < 10 )? (rolls[i + 2] + rolls[i + 3]) : (rolls[i + 2] + rolls[i + 4]));
+                } else if (firstRoll + secondRoll == 10) {
+                    result[i / 2] = rolls[i] + rolls[i + 1] + rolls[i + 2];
+                } else {
+                    result[i / 2] = rolls[i] + rolls[i + 1];
+                }
+
+                // Add result from previus step
+                if (i!=0)
+                {
+                    result[i / 2] += result[(i / 2)-1];
+                }
+            }
+            else {
+                // Last Frame
+                    result[i / 2] = rolls[i] + rolls[i + 1] + rolls[i + 2] + result[(i / 2)-1];
+               }
+        }
+        return result;
     }
 }
--- src/test/java/com/example/demo/BowlingApplicationTests.java
+++ src/test/java/com/example/demo/BowlingApplicationTests.java
@@ -9,10 +9,9 @@ class BowlingApplicationTests {
 
 	@Test
 	void contextLoads() {
-		int[] rolls = {1,4,4,5,6,4,5,5,0,10,0,1,7,3,6,4,0,10,2,8,6};
+		int[] rolls = {1,4,4,5,6,4,5,5,10,0,0,1,7,3,6,4,10,0,2,8,6};
 		int[] expectedResult = {5,14,29,49,60,61,77,97,117,133};
 		Assert.assertArrayEquals(expectedResult, BowlingGame.getResult(rolls));
-
 	}
 
 }
